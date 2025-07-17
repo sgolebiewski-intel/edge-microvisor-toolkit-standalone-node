5d0c15cbdb0a599917abe9f3e61a2f23265792df
ANAND B JYOTI <anand.b.jyoti@intel.com>
2025-07-17 14:20:21 +0530

    Remove the commented code from the scripts. (#154)
    Co-authored-by: punam20 <102661080+punam20@users.noreply.github.com>

--- standalone-node/host_os/download_tmv.sh
+++ standalone-node/host_os/download_tmv.sh
@@ -6,13 +6,9 @@
 # The file server URL is defined in FILE_RS_URL
 FILE_RS_URL="https://files-rs.edgeorchestration.intel.com"
 EMT_VERSION=3.0
-#EMT_BUILD_DATE=20250413
-#EMT_BUILD_NO=2200
 EMT_BUILD_DATE=20250711
 EMT_BUILD_NO=0415
 EMT_FILE_NAME="edge-readonly-${EMT_VERSION}.${EMT_BUILD_DATE}.${EMT_BUILD_NO}"
-#EMT_RAW_GZ="${EMT_FILE_NAME}-prod-signed.raw.gz"
-#EMT_SHA256SUM="${EMT_FILE_NAME}-prod-signed.raw.gz.sha256sum"
 EMT_RAW_GZ="${EMT_FILE_NAME}.raw.gz"
 EMT_SHA256SUM="${EMT_FILE_NAME}.raw.gz.sha256sum"
 
